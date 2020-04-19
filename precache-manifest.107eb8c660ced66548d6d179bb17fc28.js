308d379fd7d2db73e01eee41954901230564b087
AplinXy9plin <aplinxy9plin@gmail.com>
2020-04-19 14:50:24 +0700

    Updates

---
+++ precache-manifest.107eb8c660ced66548d6d179bb17fc28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16594ef68c61d93938bcb452f8a6d7d",
+    "url": "/web-app/index.html"
+  },
+  {
+    "revision": "f79c982cfe6e661220ea",
+    "url": "/web-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd589cbf4e4c060e4c48",
+    "url": "/web-app/static/js/2.b864a18d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-app/static/js/2.b864a18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79c982cfe6e661220ea",
+    "url": "/web-app/static/js/main.0eedbb5f.chunk.js"
+  },
+  {
+    "revision": "4cc65f52916c8c6c8524",
+    "url": "/web-app/static/js/runtime-main.e2ba13aa.js"
+  }
+]);
